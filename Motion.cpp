bf35fbf66764ccccf6ac21a822db44a1b8b2223d
Lee Junghwan <well0914@naver.com>
2025-11-26 15:53:04 +0900

    Solve 2 variable equation

--- Motion.cpp
+++ Motion.cpp
@@ -2,38 +2,70 @@
 #include<utility>
 using namespace std;
 
-void printMatrix(double** Matrix, int size);
+void printArray(double* arr, int size);
+void printMatrix(double** Matrix, int row, int col);
 
 int main(void) {
-	//2 x 2 matrix
-	double** A = new double* [2];
-	for (int i = 0; i < 2; i++) {
-		A[i] = new double[2];
+	int size;
+	cout << "Input size of matrix\n";
+	cin >> size;
+
+	//matrix A
+	double** A = new double* [size];
+	for (int i = 0; i < size; i++) {
+		A[i] = new double[size]();
 	}
-	A[0][0] = 1; 
-	A[0][1] = 2;
-	A[1][0] = 2;
-	A[1][1] = 1;
 
-	double C[2] = { 7, 4 };
+	cout << "Input matrix A\n";
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++)
+			cin >> A[i][j];
+	}
+
+	//Vector C
+	double* C = new double[size]();
+	cout << "Input vector C\n";
+	for (int i = 0; i < size; i++)
+		cin >> C[i];
+	
+	
+	//Swap
+	double** temp = new double* [size];
+	for (int i = 0; i < size; i++) {
+		temp[i] = new double[size]();
+	}
+	temp[0][0] = A[1][1];
+	temp[1][1] = A[0][0];
+	temp[0][1] = A[1][0] * -1;
+	temp[1][0] = A[0][1] * -1;
+
+	//Devide detA
 	double detA = A[0][0] * A[1][1] - A[0][1] * A[1][0];
-	
-	//Adj
-	swap(A[0][0], A[1][1]);
-	swap(A[0][1], A[1][0]);
-	A[0][1] *= -1;
-	A[1][0] *= -1;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++)
+			temp[i][j] /= detA;
+	}
 
-	printMatrix(A, 2);
-	
-	cout << "detA = " << detA << "\n";
+	//Solve
+	double* B = new double[size]();
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++)
+			B[i] += temp[i][j] * C[j];
+	}
+
+	printArray(B, size);
 
 	return 0;
 }
 
-void printMatrix(double** Matrix, int size) {
-	for (int i = 0; i < size; i++) {
-		for(int j = 0; j < size; j++)
+void printArray(double* arr, int size) {
+	for (int i = 0; i < size; i++) 
+		cout << arr[i] << " ";
+}
+
+void printMatrix(double** Matrix, int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for(int j = 0; j < col; j++)
 			cout << Matrix[i][j] << " ";
 		cout << "\n";
 	}
